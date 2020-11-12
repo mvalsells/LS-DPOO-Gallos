ce984e409946df8c42afc3313c0895c89a93775d
Marc Valsells <marc@valsells.me>
2020-11-12 12:04:45 +0100

    filieToString OK

--- src/Json.java
+++ src/Json.java
@@ -1,18 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonPrimitive;
+
+import java.io.IOException;
 import java.util.Map.Entry;
 
 public class Json {
-    public static void main(String args[]) throws java.io.IOException {
+
+
+    public String fileToString (String fileName) throws IOException {
+
+        FileReader fr;
+        BufferedReader br;
+        StringBuilder sb;
+        String line;
+        fr = new FileReader(fileName);
+        br = new BufferedReader(fr);
+        sb = new StringBuilder();
+        line = br.readLine();
+
+        while (line != null ){
+            sb.append(line);
+            line = br.readLine();
+        }
+
+        return sb.toString();
+    }
+
+    /*public static void main(String args[]) throws java.io.IOException {
         JsonParser parser = new JsonParser();
-        FileReader fr = new FileReader("competició.json");
+        FileReader fr = new FileReader("competicio.json");
         JsonElement datos = parser.parse(fr);
         dumpJSONElement(datos);
-    }
+    }*/
 
     public static void dumpJSONElement(JsonElement element){
         //El método «dumpJSONElement» debe determinar el tipo de elemento que
@@ -23,6 +48,8 @@ public class Json {
 
 
     }
+
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Main  {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Creem el taulell, el seu controller i el Menú (Interfície Gràfica)
-        Menu menu;
+        /*Menu menu;
         Competicio competicio;
         ControllerCompeticio controllerCompeticio;
 
@@ -14,8 +16,13 @@ public class Main  {
         controllerCompeticio = new ControllerCompeticio(competicio, menu);
         // Executem el bucle infinit del programa, el qual serà gestionat pel controller
         //taulellController.comencaCompeticio();//cosa de Malé!!!!!!
-        controllerCompeticio.executaMenu();
+        controllerCompeticio.executaMenu();*/
 
+        Json json;
+
+        json = new Json();
+
+        System.out.print(json.fileToString("src/competicio.json"));
     }
 
 }
