fd34105324b9f6867025a5c098287215759f506a
Albert Clarimon <albert.clarimon@students.salle.url.edu>
2020-11-12 09:32:42 +0100

    json llibreria posada

--- src/Json.java
+++ src/Json.java
@@ -1,4 +1,16 @@
-public class Json {
+import java.io.FileReader;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonPrimitive;
+import java.util.Map.Entry;
 
+public class Json {
+    public static void main(String args[]) throws java.io.IOException {
+        JsonParser parser = new JsonParser();
+        FileReader fr = new FileReader("datos.json");
+        JsonElement datos = parser.parse(fr);
+        dumpJSONElement(datos);
 
 }
