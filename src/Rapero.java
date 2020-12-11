9a3a9a734f5d2458747fd26bfdbfcb63b363d7a1
albert clarimon <albert.clarimon@students.salle.url.edu>
2020-12-11 12:42:40 +0100

    javadoc Rapero ok

--- src/Rapero.java
+++ src/Rapero.java
@@ -1,6 +1,14 @@
+
+/**
+ * Esta clase nos permite controlar el poryecto en su totalidad, tanto en el momento de ejecutar el menú, como en el de crear el controller.
+ *
+ * @ author: Marc Valsells y Albert Clarimón.
+ * @ version: 10/12/2020.
+ */
+
 public class Rapero {
 
-    //Atributs
+    //Campos de la clase
     private String realName;
     private String stageName;
     private String  birth;
@@ -9,7 +17,17 @@ public class Rapero {
     private String photo;
     private double puntuacio;
 
-    //Constructor
+    /**
+     * Constructor de Rapero
+     *
+     * @param realName    El parámetro realName nos indica el nombre real del rapero
+     * @param stageName   El parámetro stageName nos indica el nombre artístico del rapero.
+     * @param birth       El parámetro birth nos indica la fecha de nacimiento del rapero.
+     * @param nationality El parámetro nationality nos indica la nacionalidad del rapero.
+     * @param level       El parámetro level indica cual es el level del usuario.
+     * @param photo       El parámetro photo nos indica cual es la foto del rapero.
+     */
+
     public Rapero(String realName, String stageName, String birth, String nationality, int level, String photo) {
         this.realName = realName;
         this.stageName = stageName;
@@ -18,25 +36,53 @@ public class Rapero {
         this.level = level;
         this.photo = photo;
         puntuacio = 0.0f;
-    }
+    }//Cierre del constructor
+
     //Getters and Setters
+
+    /**
+     * Método que devuelve el nombre artístico del rapero.
+     *
+     * @return El nombre artístico del rapero.
+     */
     public String getStageName() {
         return stageName;
-    }
+    }//Cierre del método
+
+    /**
+     * Método que devuelve el nivel del rapero.
+     *
+     * @return El nivel del rapero.
+     */
 
     public int getLevel() {
         return level;
-    }
+    }//Cierre del método
+
+    /**
+     * Método que devuelve la puntuaciçón del rapero.
+     *
+     * @return La puntuación del rapero.
+     */
 
     public double getPuntuacio() {
         return puntuacio;
-    }
+    }//Cierre del método
 
+    /**
+     * Método que coje la puntuaciçón del rapero.
+     */
     public void setPuntuacio(double puntuacio) {
         this.puntuacio = puntuacio;
-    }
+    }//Cierre del método
 
     //Metodes
+
+    /**
+     * Método que compara la puntuación del rapero.
+     * @param rapero El parámetro rapero contiende la información de rapero.
+     * @return La puntuación del rapero.
+     */
     public double comparePuntuacio(Rapero rapero) {
         return this.getPuntuacio()-rapero.getPuntuacio();
     }
