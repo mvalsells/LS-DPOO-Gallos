26f2600146ed1d420c7bd47778549d5f7c73c742
albert clarimon <albert.clarimon@students.salle.url.edu>
2020-12-11 12:26:26 +0100

    javadoc Main ok

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,26 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Esta clase nos permite controlar el poryecto en su totalidad, tanto en el momento de ejecutar el menú, como en el de crear el controller.
+ *
+ * @ author: Marc Valsells y Albert Clarimón.
+ * @ version: 10/12/2020.
+ */
+
 public class Main {
+
+    /**
+     * Método que controla el proyecto, permite crear el controller y ejecutar el menú
+     * @param args El parámetro se encarga de ejecutar los argumentos del programa.
+     * @throws IOException El parámetro se encargra de si el programa falla por algo no se salga de la ejecución.
+     * @throws InterruptedException El parámetro se encarga de si hay un fallo en el tiempo.
+     */
 
     public static void main(String[] args) throws IOException, InterruptedException {
         ControllerCompeticio controllerCompeticio = new ControllerCompeticio();
 
         controllerCompeticio.executaMenu();
-    }
+    }//Cierre del método
 
-}
+}//Cierre de la clase Main
