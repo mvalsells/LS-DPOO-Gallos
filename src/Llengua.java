564a249815c2a63eaf2a581217763de603a48960
albert clarimon <albert.clarimon@students.salle.url.edu>
2020-12-11 12:21:39 +0100

    javadoc LLnegua ok

--- src/Llengua.java
+++ src/Llengua.java
@@ -1,7 +1,21 @@
+
+/**
+ * Esta clase nos permite controlar el idioma con el que se hará la batalla.
+ *
+ * @ author: Marc Valsells y Albert Clarimón.
+ * @ version: 10/12/2020.
+ */
+
 public class Llengua {
+
+    //Campos de la clase
     private String nomNatiu;
     private String nomAngles;
 
+    /**
+     * Método que dirá con que lengua se ejecuta la batalla
+     */
+
     public Llengua() {
-    }
-}
+    }//Cierre del método
+}//Cierre de la clase LLengua
