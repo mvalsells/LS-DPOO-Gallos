de5de03a9e71d81f85bfde5503e4b68136f1f786
albert clarimon <albert.clarimon@students.salle.url.edu>
2020-12-11 12:19:10 +0100

    javadoc Json ok

--- src/Json.java
+++ src/Json.java
@@ -4,21 +4,36 @@ import java.io.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Esta clase nos permite tanto leer del json como escribir en él.
+ *
+ * @ author: Marc Valsells y Albert Clarimón.
+ * @ version: 10/12/2020.
+ */
 
 public class Json {
 
-    //Atributs
+    //Campos de la classe
     private final String fitxerCompeticio;
     private final String fitxerBatalla;
 
-    //Constructor
+    /**
+     * Constructor de Json
+     * @param fitxerCompeticio El parametro fitxerCompeticio, contiene el fichero json de la competición.
+     * @param fitxerBatalla El parametro fitxerBatalla, contiene el fichero json de la batalla.
+     */
+
     public Json(String fitxerCompeticio, String fitxerBatalla) {
         this.fitxerCompeticio = fitxerCompeticio;
         this.fitxerBatalla = fitxerBatalla;
-    }
+    }//Cierre del constructor
 
     //Mètodes
 
+    /**
+     * Método por el cual se lee el json de competición, una vez leido se separará en competition, countries o rappers.
+     * @return El conntenido que tiene cada competición.
+     */
 
     public Competicio llegirCompeticio() {
         try {
@@ -115,9 +130,15 @@ public class Json {
             return null;
         }
 
-    }
+    }//Cierre del método
 
     //Llegir temes
+
+    /**
+     * Método que se utiliza al momento de leer los temas y estrofas que hay en el fichero de batalla.
+     * @return El array de temas donde contiene todos los temas y estrofas.
+     */
+
     public ArrayList<Tema> llegirTema() {
         try {
             //Atributs llegir JSON
@@ -165,11 +186,19 @@ public class Json {
             System.exit(1);
             return null;
         }
-    }
+    }//Cierre del método
+
+    /**
+     * Método que se usa al momento de escribir un rapero al json, cuando este se inscirbe en la competición.
+     * @param realName    El parámetro realName nos indica el nombre real del rapero
+     * @param stageName   El parámetro stageName nos indica el nombre artístico del rapero.
+     * @param birth       El parámetro birth nos indica la fecha de nacimiento del rapero.
+     * @param nationality El parámetro nationality nos indica la nacionalidad del rapero.
+     * @param level       El parámetro level indica cual es el level del usuario.
+     * @param photo       El parámetro photo nos indica cual es la foto del rapero.
+     */
 
     public void escriureRapero(String realName, String stageName, String birth, String nationality, int level, String photo) {
-
-
 
         GsonBuilder builder = new GsonBuilder();
         builder.setPrettyPrinting().serializeNulls();
@@ -214,5 +243,5 @@ public class Json {
             System.exit(1);
         }
 
-    }
-}
+    }//Cierre del método
+}//Cierre de la clase Json
