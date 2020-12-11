74b3993aec93b898617128d962ef3724dd473ff4
albert clarimon <albert.clarimon@students.salle.url.edu>
2020-12-11 12:36:21 +0100

    javadoc Pais ok

--- src/Pais.java
+++ src/Pais.java
@@ -1,7 +1,14 @@
 import java.net.URL;
 
+/**
+ * Esta clase nos permite controlar toda la información que tiene el pais.
+ *
+ * @ author: Marc Valsells y Albert Clarimón.
+ * @ version: 10/12/2020.
+ */
 public class Pais {
-    //Atributs
+
+    //Campos de la clase
     private String nomAngles;
     private int habitants;
     private String regio;
@@ -10,12 +17,23 @@ public class Pais {
 
 
     //Constructor
+
+    /**
+     * Constructor de Pais
+     * @param nomAngles El parámetro de nomAngles nos traduce el nombre del pais al inglés.
+     */
     public Pais(String nomAngles){
         this.nomAngles=nomAngles;
-    }
+    }//Cierre del constructor
 
     //Getters and Setters
+
+    /**
+     * Método que nos coge el nombre en inglés del pais.
+     * @return El nombre en inglés del pais.
+     */
+
     public String getNomAngles() {
         return nomAngles;
-    }
-}
+    }//Cierre del método
+}//Cierre de la clase Pais
